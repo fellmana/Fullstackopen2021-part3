2d07fbed0980643bd641811666d2aacfc510d404
Aslak <aslak.fellman@helsinki.fi>
2021-05-08 17:05:21 +0300

    testing

--- index.js
+++ index.js
@@ -22,13 +22,17 @@ app.get('/api/persons', (request,response) => {
 })
 
 app.get('/api/persons/:id', (request,response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    const person = persons.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => {
+        console.log(error)
+        response.status(500).end()
+    })
 })
 
 app.get('/info', (request,response) => {
